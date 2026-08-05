d654fefe77352e3eb8170c63f7a7037cfeb98a1f
Somendra Kothari <ch25btech11041@iith.ac.in>
2026-08-05 22:08:14 +0530

    Time: 228 ms (94.48%), Space: 20.8 MB (72.09%) - LeetHub

--- 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.py
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.py
@@ -5,25 +5,36 @@ class Solution(object):
         :type k: int
         :rtype: int
         """
+        # n.sort()
+        # l=0
+        # s=0
+        # m=0
+        # for r in range(1,len(n)):
+        #     if n[r]==n[r-1]:
+        #         if m<r-l+1:
+        #             m=r-l+1
+        #         continue
+        #     s+=(n[r]-n[r-1])*(r-l)
+        #     # print(s,r)
+        #     if s==k:
+        #         if m<r-l+1:
+        #             m=r-l+1
+        #     while l<r and s>k:
+        #         s-=n[r]-n[l]
+        #         l+=1
+        #         # print(2)
+        # if m<len(n)-l:
+        #     # print(1)
+        #     m=len(n)-l
+        # return m
+
+        # non shrinking sw
         n.sort()
         l=0
         s=0
-        m=0
         for r in range(1,len(n)):
-            if n[r]==n[r-1]:
-                if m<r-l+1:
-                    m=r-l+1
-                continue
             s+=(n[r]-n[r-1])*(r-l)
-            # print(s,r)
-            if s==k:
-                if m<r-l+1:
-                    m=r-l+1
-            while l<r and s>k:
+            if s>k:
                 s-=n[r]-n[l]
                 l+=1
-                # print(2)
-        if m<len(n)-l:
-            # print(1)
-            m=len(n)-l
-        return m
+        return len(n)-l
