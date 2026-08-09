eb1e00fbeb4aa582cd548c29785779182f9b24a1
Somendra Kothari <ch25btech11041@iith.ac.in>
2026-08-09 13:50:55 +0530

    Time: 0 ms (100%), Space: 13.6 MB (73.79%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -4,6 +4,7 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        l=len(nums)
         for i in list(set(nums)):
-            if nums.count(i) > len(nums)/2:
+            if nums.count(i) > l/2:
                 return i
