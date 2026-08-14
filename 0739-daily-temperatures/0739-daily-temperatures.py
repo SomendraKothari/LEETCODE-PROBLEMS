8b7e46d54b24a3c62f1440689bdf538f79b48b90
Somendra Kothari <ch25btech11041@iith.ac.in>
2026-08-14 15:10:15 +0530

    Time: 149 ms (89.8%), Space: 24.6 MB (89.27%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -28,13 +28,28 @@ class Solution(object):
         # return ans
 
 
-        l=len(t)
-        s=[]
-        ans=[0]*l
-        for i in range(l-1,-1,-1):
-            while len(s)>0 and t[s[-1]]<=t[i]:
-                s.pop()
-            if s:
-                ans[i]=s[-1]-i
-            s.append(i)
+        # l=len(t)
+        # s=[]
+        # ans=[0]*l
+        # for i in range(l-1,-1,-1):
+        #     while len(s)>0 and t[s[-1]]<=t[i]:
+        #         s.pop()
+        #     if s:
+        #         ans[i]=s[-1]-i
+        #     s.append(i)
+        # return ans
+
+
+        n = len(t)
+        ans = [0] * n
+        stack = []  # Stack mein indices rahenge
+
+        for i, temp in enumerate(t):
+        # Jab tak current temperature stack ke top wale temperature se bada hai
+            while stack and t[stack[-1]] < temp:
+                prev_idx = stack.pop()
+                ans[prev_idx] = i - prev_idx
+            
+            stack.append(i)
+
         return ans
