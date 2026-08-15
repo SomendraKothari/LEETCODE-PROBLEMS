c261e59f21aee514f1147e2e1fb314e35bcc8085
Somendra Kothari <ch25btech11041@iith.ac.in>
2026-08-15 18:52:13 +0530

    Time: 0 ms (100%), Space: 12.6 MB (27.93%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def fp(self,x,n):
+        if n==0:
+            return 1
+        a=self.fp(x,n//2)
+        if n%2==0:
+            return a*a
+        else:
+            return a*a*x
+    def myPow(self, x, n):
+        """
+        :type x: float
+        :type n: int
+        :rtype: float
+        """
+        if n>=0:
+            return self.fp(x,n)
+        else:
+            return 1/self.fp(x,-n)
