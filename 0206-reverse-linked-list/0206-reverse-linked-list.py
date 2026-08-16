ba6e8078d8c8ce2332a816337fcc8a41c56bcabe
Somendra Kothari <ch25btech11041@iith.ac.in>
2026-08-16 17:04:48 +0530

    Time: 0 ms (100%), Space: 18.2 MB (5.28%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -4,16 +4,25 @@
 #         self.val = val
 #         self.next = next
 class Solution(object):
+    def rev(self,c,n,p):
+        if c==None:
+            return p
+        n=c.next
+        c.next=p
+        return self.rev(n,n,c)
     def reverseList(self, head):
         """
         :type head: Optional[ListNode]
         :rtype: Optional[ListNode]
         """
-        p=n=None
-        c=head
-        while c:
-            n=c.next
-            c.next=p
-            p=c
-            c=n
-        return p
+        # p=n=None
+        # c=head
+        # while c:
+        #     n=c.next
+        #     c.next=p
+        #     p=c
+        #     c=n
+        # return p
+
+        return self.rev(head,None,None)
+
