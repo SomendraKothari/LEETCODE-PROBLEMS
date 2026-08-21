6fa9db51299a14d6c3991eca62407b1a4781dfa4
Somendra Kothari <ch25btech11041@iith.ac.in>
2026-08-21 15:23:45 +0530

    Time: 15 ms (33.07%), Space: 20 MB (7.18%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -5,12 +5,43 @@ class Solution(object):
         :type k: int
         :rtype: List[int]
         """
+        # d={}
+        # for i in n:
+        #     d[i]=d.get(i,0)+1
+        # l=[[] for i in range(len(n)+1)]
+        # for i in d.items():
+        #     l[i[1]].append(i[0])
+        # ans=[]
+        # for i in range(len(n),0,-1):
+        #     for m in l[i]:
+        #         ans.append(m)
+        #         if len(ans)==k:
+        #             return ans
+
+
+        # d={}
+        # for i in n:
+        #     d[i]=d.get(i,0)+1
+        # # d={1:3,2:2,3:1}
+        # l=[]
+        # for _ in range(len(n)+1):
+        #     l.append([])
+        # for i,j in d.items():
+        #     l[j].append(i)
+        # r=[]
+        # for i in range(len(n),0,-1):
+        #     for j in l[i]:
+        #         r.append(j)
+        #         if len(r)==k:
+        #             return r
+
+
         d={}
         for i in n:
             d[i]=d.get(i,0)+1
         l=[[] for i in range(len(n)+1)]
-        for i,j in d.items():
-            l[j].append(i)
+        for i,m in d.items():
+            l[m].append(i)
         ans=[]
         for i in range(len(n),0,-1):
             for j in l[i]:
