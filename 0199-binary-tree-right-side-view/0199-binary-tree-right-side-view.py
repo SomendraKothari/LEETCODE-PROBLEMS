d0dc617052f48fd5ae3949b76df2fa8aa2bfebbb
Somendra Kothari <ch25btech11041@iith.ac.in>
2026-08-28 19:47:41 +0530

    Time: 0 ms (100%), Space: 12.5 MB (6.25%) - LeetHub

---
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.py
@@ -0,0 +1,53 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class que():
+    def __init__(self):
+        self.q=[]
+        self.f=-1
+    def push(self,x):
+        if self.f==-1:
+            self.f=0
+        self.q.append(x)
+    def pop(self):
+        if len(self.q)==0:
+            return -1
+        x=self.q[self.f]
+        self.f+=1
+        if self.f==len(self.q):
+            self.f=-1
+            self.q=[]
+        return x
+    def size(self):
+        if self.f==-1:
+            return 0
+        return len(self.q)-self.f 
+class Solution(object):
+    def rightSideView(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: List[int]
+        """
+        ans=[]
+        if not root:
+            return ans
+        q=que()
+        q.push(root)
+        ans.append(root.val)
+        while q.size()>0:
+            l=q.size()
+            le=[]
+            for i in range(l):
+                x=q.pop()
+                if x.left:
+                    q.push(x.left)
+                    le.append(x.left.val)
+                if x.right:
+                    q.push(x.right)
+                    le.append(x.right.val)
+            if len(le)>0:
+                ans.append(le[-1])
+        return ans
