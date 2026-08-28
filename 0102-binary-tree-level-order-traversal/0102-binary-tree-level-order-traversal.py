04f147dcc530b131650708085239195fdc7d32ac
Somendra Kothari <ch25btech11041@iith.ac.in>
2026-08-28 18:29:15 +0530

    Time: 0 ms (100%), Space: 13.3 MB (10.94%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -3,28 +3,28 @@
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
-#         self.right = right
-class que():
+#         self.right = 
+class Queue(object):
     def __init__(self):
         self.q=[]
-        self.f=-1
+        self.front=-1
     def push(self,x):
-        if self.f==-1:
-            self.f=0
+        if self.front==-1:
+            self.front=0
         self.q.append(x)
     def pop(self):
         if len(self.q)==0:
             return -1
-        x=self.q[self.f]
-        self.f+=1
-        if self.f==len(self.q):
-            self.f=-1
+        x=self.q[self.front]
+        self.front+=1
+        if self.front==len(self.q):
             self.q=[]
+            self.front=-1
         return x
     def size(self):
-        if self.f==-1:
+        if self.front==-1:
             return 0
-        return len(self.q)-self.f
+        return len(self.q)-self.front
 class Solution(object):
     def levelOrder(self, root):
         """
@@ -32,22 +32,22 @@ class Solution(object):
         :rtype: List[List[int]]
         """
         ans=[]
-        if not root:
+        if root==None:
             return ans
-        q=que()
-        ans.append([root.val])
+        q=Queue()
         q.push(root)
+        ans.append([root.val])
         while q.size()>0:
-            level=[]
             l=q.size()
+            lev=[]
             for i in range(l):
                 x=q.pop()
                 if x.left:
                     q.push(x.left)
-                    level.append(x.left.val)
+                    lev.append(x.left.val)
                 if x.right:
                     q.push(x.right)
-                    level.append(x.right.val)
-            if len(level)>0:
-                ans.append(level)
+                    lev.append(x.right.val)
+            if len(lev)!=0:
+                ans.append(lev)
         return ans
