2391ab56d1db10e4f050c276bee67e4263988f89
Somendra Kothari <ch25btech11041@iith.ac.in>
2026-09-19 18:34:48 +0530

    Time: 0 ms (100%), Space: 14.3 MB (33.85%) - LeetHub

--- 0112-path-sum/0112-path-sum.py
+++ 0112-path-sum/0112-path-sum.py
@@ -11,16 +11,23 @@ class Solution(object):
         :type targetSum: int
         :rtype: bool
         """
-        self.ans=False
-        def check(root,s):
-            if not root:
-                return 0
-            if self.ans:
-                return
-            s+=root.val
-            l=check(root.left,s)
-            r=check(root.right,s)
-            if l==0 and r==0 and s==t:
-                self.ans=True
-        check(root,0)
-        return self.ans
+        # self.ans=False
+        # def check(root,s):
+        #     if not root:
+        #         return 0
+        #     if self.ans:
+        #         return
+        #     s+=root.val
+        #     l=check(root.left,s)
+        #     r=check(root.right,s)
+        #     if l==0 and r==0 and s==t:
+        #         self.ans=True
+        # check(root,0)
+        # return self.ans
+
+        if not root:
+            return False
+        if not root.left and not root.right:
+            return t==root.val
+        rem=t-root.val
+        return self.hasPathSum(root.left,rem) or self.hasPathSum(root.right,rem)
