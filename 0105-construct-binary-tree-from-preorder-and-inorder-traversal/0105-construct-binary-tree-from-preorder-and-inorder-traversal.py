679a0e355a0c27537dab03f769e9d2443e684762
Somendra Kothari <ch25btech11041@iith.ac.in>
2026-09-20 23:53:03 +0530

    Time: 5 ms (86.54%), Space: 16.9 MB (88.31%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -13,8 +13,8 @@ class Solution(object):
         """
         l=len(p)
         d={}
-        for ind,v in enumerate(i):
-            d[v]=ind
+        for j in range(l):
+            d[i[j]]=j
         def r(p,ps,pe,i,iss,ie):
             if ps>pe or iss>ie:
                 return None
